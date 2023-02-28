d015cabf424282a1294b120dc8e34793766dc53b
tringapps-rohith <123439228+tringapps-rohith@users.noreply.github.com>
2023-02-28 14:00:56 +0530

    Update StudentList.java

--- src/main/java/studentlist/studentlist/StudentList.java
+++ src/main/java/studentlist/studentlist/StudentList.java
@@ -6,116 +6,11 @@ package studentlist.studentlist;
  */
 import java.util.*;
 import java.util.logging.*;
-class Student
-{
-	public static final String COM="com.api.jar";
-     String name;
-    String email;
-    long phno;
-    public Student(String name,String email,long phno)
-    {
-            this.name=name;
-            this.email=email;
-            this.phno=phno;
-    }
-					Logger l=Logger.getLogger(COM);
 
-    public Student()
-    {
-    }
-    public void setName(String name)
-    {
-            this.name=name;
-    }
-    public String getName()
-    {
-            return this.name;
-    }
-
-    public void setEmail(String email)
-    {
-            this.email=email;
-    }
-    public String getEmail()
-    {
-            return this.email;
-    }
-    public void setPhno(long phno)
-
-    {
-            this.phno=phno;
-    }
-    public long getPhno()
-    {
-            return this.phno;
-    }
-    public String toString()
-    {
-            return getName()+" "+getEmail()+" "+getPhno();
-    }
-    Hash<Long,String>hs=new Hashtable<>();
-     Scanner sin=new Scanner(System.in);
-       void getDetails()
-        {
-                l.info("\nEnter student name\n");
-                String n=sin.next();
-                l.info("Enter email id\n");
-                String e=sin.next();
-                l.info("Enter phone no\n");
-                long ph=sin.nextLong();
-                setName(n);
-                setEmail(e);
-                setPhno(ph);
-         
-
-        }
-        void displayDetails()
-        {
-                l.log(Level.INFO,()-> "\nStudent name "+getName());
-                l.log(Level.INFO,()-> "\nMail id "+getEmail());
-                l.log(Level.INFO,()-> "\nPhone no "+getPhno());
-        }
-        void hashAdd()
-        {
-                getDetails();
-                hs.put(getPhno(),getName());
-                l.log(Level.INFO,()->""+ hs);
-        }
-        void hashRemove()
-        {
-                l.info("Enter phno\n");
-                long pno=sin.nextLong();
-                hs.remove(pno);
-                l.log(Level.INFO,()->""+ hs);
-        }
-
-}
-class Node
-{
-        Student data;
-        Node next;
-						
-
-        public Node(Student data,Node next)
-        {
-                this.data=data;
-                this.next=next;
-        }
-        public Node()
-        {
-                this.data=new Student();
-                next=null;
-        }
-        void dataDetails()
-        {
-                data.getDetails();
-        }
-      
-}
 class StudentList
 {
         Node root;
-						Logger l=Logger.getLogger(COM);
+	Logger l=Logger.getLogger(COM);
 
         int insertAtFirst(Node newNode)
         {
